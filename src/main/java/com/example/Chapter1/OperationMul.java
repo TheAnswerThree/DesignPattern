7467b8026413d827ac4b43e8c541db5a2c7f6740
563092525 <563092525@qq.com>
2021-10-27 16:32:20 +0800

    Changes

--- src/main/java/com/example/Chapter1/OperationMul.java
+++ src/main/java/com/example/Chapter1/OperationMul.java
@@ -5,6 +5,7 @@ public class OperationMul extends Operation {
     public double GetResult() {
         double result = 0;
         result = get_numberA() * get_numberB();
+        String a="";
         return result;
     }
 }
