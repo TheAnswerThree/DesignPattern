95ad62d270e3d1cf145717744586f46f4c0d285b
563092525@qq.com <54572830jia>
2021-10-28 23:01:58 +0800

    就试试

--- src/main/java/com/example/Chapter1/OperationMul.java
+++ src/main/java/com/example/Chapter1/OperationMul.java
@@ -5,7 +5,6 @@ public class OperationMul extends Operation {
     public double GetResult() {
         double result = 0;
         result = get_numberA() * get_numberB();
-        String a="好使吗？？";
         return result;
     }
 }
